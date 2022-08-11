ef3e53651aa8fff39bc65481058f1cd3ce446a7b
HP <HP@DESKTOP-LAIIRTJ>
2022-08-11 10:46:23 +0000

    fistcommit

---
+++ src/main/java/com/example/demo/ProjetannonceApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetannonceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetannonceApplication.class, args);
+	}
+
+}
